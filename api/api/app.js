6d05d90ffe37ef234ecc243f098f1e350cb66cf5
pacman29 <zx19-95@mail.ru>
2017-12-03 19:08:46 +0300

    serve-static

--- api/api/app.js
+++ api/api/app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var serveStatic = require('serve-static');
 
 var index = require('./routes/index');
 var system = require('./routes/system');
@@ -22,7 +23,8 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+//app.use(express.static(path.join(__dirname, 'public')));
+app.use(serveStatic(path.join(__dirname, 'public')))
 
 
 
